7c8f38819a06770af3906a096b2c356e8b50d3f3
Skachkov,Viktor V.A <v.skachkov@student.fontys.nl>
2021-06-08 13:01:13 +0000

    Upload New File

---
+++ Main_Project/Complaint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_House
+{
+    class Complaint
+    {
+        public static List<string[]> complaintlist = new List<string[]>();
+        private string description;
+        private DateTime time;
+
+        public Complaint(string description, DateTime time)
+        {
+            complaintlist.Add(new string[] { description, time.ToString() });
+        }
+
+        public static void RemoveCom(int index)
+        {
+            Complaint.complaintlist.RemoveAt(index);
+        }
+        public static List<string[]> GetComplaints()
+        {
+            return complaintlist;
+        }
+    }
+}
