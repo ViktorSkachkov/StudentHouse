172203719cde96c155bd402a11a33ece66911f67
Skachkov,Viktor V.A <v.skachkov@student.fontys.nl>
2021-06-08 13:04:01 +0000

    Upload New File

---
+++ Main_Project/LogIn.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Student_House
+{
+    partial class LogIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.tbUserNumber = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.lblUserNumber = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.signIn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogIn.Location = new System.Drawing.Point(360, 245);
+            this.btnLogIn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(259, 94);
+            this.btnLogIn.TabIndex = 0;
+            this.btnLogIn.Text = "Log In";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
+            // 
+            // tbUserNumber
+            // 
+            this.tbUserNumber.Location = new System.Drawing.Point(360, 81);
+            this.tbUserNumber.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbUserNumber.Name = "tbUserNumber";
+            this.tbUserNumber.Size = new System.Drawing.Size(259, 22);
+            this.tbUserNumber.TabIndex = 3;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(360, 174);
+            this.tbPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(259, 22);
+            this.tbPassword.TabIndex = 7;
+            // 
+            // lblUserNumber
+            // 
+            this.lblUserNumber.AutoSize = true;
+            this.lblUserNumber.Location = new System.Drawing.Point(357, 50);
+            this.lblUserNumber.Name = "lblUserNumber";
+            this.lblUserNumber.Size = new System.Drawing.Size(96, 17);
+            this.lblUserNumber.TabIndex = 8;
+            this.lblUserNumber.Text = "User Number:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(357, 142);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(73, 17);
+            this.lblPassword.TabIndex = 11;
+            this.lblPassword.Text = "Password:";
+            // 
+            // signIn
+            // 
+            this.signIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signIn.Location = new System.Drawing.Point(360, 412);
+            this.signIn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.signIn.Name = "signIn";
+            this.signIn.Size = new System.Drawing.Size(259, 49);
+            this.signIn.TabIndex = 13;
+            this.signIn.Text = "Create new account";
+            this.signIn.UseVisualStyleBackColor = true;
+            this.signIn.Click += new System.EventHandler(this.signIn_Click);
+            // 
+            // LogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1012, 523);
+            this.Controls.Add(this.signIn);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUserNumber);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUserNumber);
+            this.Controls.Add(this.btnLogIn);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "LogIn";
+            this.Text = "Log In Page";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.TextBox tbUserNumber;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label lblUserNumber;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button signIn;
+    }
+}
+
