9f5f0d84fc8308df750248a93dff639b26dce9e9
Skachkov,Viktor V.A <v.skachkov@student.fontys.nl>
2021-06-08 13:04:50 +0000

    Upload New File

---
+++ Main_Project/User.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_House
+{
+    public class User
+    {
+        private int userNumber;
+        private String firstName;
+        private String surname;
+        private String lastName;
+        private String email;
+        private String password;
+        private String determinePassword;
+        private bool pending;
+        private bool banned;
+        public User(int userNumber, String firstName, String surname, String lastName, String email, String password, String determinePassword, bool pending, bool banned)
+        {
+            this.userNumber = userNumber;
+            this.firstName = firstName;
+            this.surname = surname;
+            this.lastName = lastName;
+            this.email = email;
+            this.password = password;
+            this.determinePassword = determinePassword;
+            this.pending = pending;
+            this.banned = banned;
+        }
+        public int UserNumber
+        {
+            get { return this.userNumber; }
+        }
+        public String FirstName
+        {
+            get { return this.firstName; }
+        }
+        public String Surname
+        {
+            get { return this.surname; }
+        }
+        public String LastName
+        {
+            get { return this.lastName; }
+        }
+        public String Email
+        {
+            get { return this.email; }
+        }
+        public String Password
+        {
+            get { return this.password; }
+        }
+        public String DeterminePassword
+        {
+            get { return this.determinePassword; }
+        }
+        public bool Pending
+        { get { return this.pending; }
+            set { this.pending = value; }
+        }
+
+        public bool Banned
+        {
+            get { return this.banned; }
+            set { this.banned = value; }
+        }
+
+    }
+}
